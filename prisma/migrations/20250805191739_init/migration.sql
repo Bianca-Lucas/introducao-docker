71bd48d2a13a63626d33077036d2560a4c3eefa0
Bianca Lucas <biaxx.iluks@gmail.com>
2025-08-06 15:45:42 -0300

    chore: files of config init

---
+++ prisma/migrations/20250805191739_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateEnum
+CREATE TYPE "public"."BookCategory" AS ENUM ('ROMANCE', 'FANTASIA', 'SUSPENSE', 'TERROR', 'BIOGRAFIA', 'INFANTIL', 'RELIGIOSO');
+
+-- CreateTable
+CREATE TABLE "public"."Book" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "publicationDate" TEXT,
+    "category" "public"."BookCategory",
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { BookModule } from './book/book.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BookModule, PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
